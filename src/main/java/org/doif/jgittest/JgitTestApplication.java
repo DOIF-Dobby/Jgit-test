eb5ecc9a2f734dd624df96231f088a702258d2bc
doif-dobby <doif.dobby@gmail.com>
2020-12-22 14:59:51 +0900

    first commit

---
+++ src/main/java/org/doif/jgittest/JgitTestApplication.java
@@ -0,0 +1,13 @@
+package org.doif.jgittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JgitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/doif/jgittest/JgitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.doif.jgittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
